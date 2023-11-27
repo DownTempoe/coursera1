2037b5a2fc2ec78d989d83e716eae1450e42e56d
Rudyard Pue <44810054+DownTempoe@users.noreply.github.com>
2023-11-26 22:44:29 -0500

    Update stats.c
    Added project outline

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <Coursera1> 
+ * @brief <Assignment 1 for Embedded Systems Coursera course. Simple application that performs
+ *         statistical analytics on a a dataset.>
+ * 
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Rudyard Pue>
+ * @date <26 November 2023 >
  *
  */
 
@@ -27,6 +27,28 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
+
+//Function that prints the statistics of an array including minimum, maximum, mean, and median.
+void print_statistics(int test[], int min, int max, int mean, int median){}
+
+//Function prints the array to the screen
+void print_array(int test[]){}
+
+//Function returns the median value of an array
+int find_median(int test[]){}
+
+//Function returns the mean of an array
+int find_mean(int test[]){}
+
+//Function returns the maximum of an array
+int find_maximum(int test[]){}
+
+//Function returns the minimum of an array
+int find_minimum(int test[]){}
+
+//Function sorts the array from largest to smallest
+void sort_array(int test[]){}
+
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -35,9 +57,5 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
 
 }
-
-/* Add other Implementation File Code Here */
